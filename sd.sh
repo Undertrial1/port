d7776ddb5ef149468324958ed9bb81fcee6c8ea8
fintafixuser <164501103+fintafixuser@users.noreply.github.com>
2024-04-17 16:30:51 +0300

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,18 +1,28 @@
 import psutil
 
-def get_multi_core_processes():
-    multi_core_processes = []
-    for proc in psutil.process_iter(['pid', 'name', 'cpu_affinity', 'exe']):
-        cpu_affinity = proc.info['cpu_affinity']
-        if cpu_affinity and len(cpu_affinity) > 2:
-            multi_core_processes.append(proc)
-    return multi_core_processes
+def get_linux_processes():
+    try:
+        # Получаем список всех процессов
+        all_processes = psutil.process_iter()
+        process_list = []
+        for process in all_processes:
+            try:
+                # Получаем информацию о каждом процессе и добавляем в список
+                process_info = process.as_dict(attrs=['pid', 'name', 'username', 'cpu_percent', 'memory_percent'])
+                process_list.append(process_info)
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return process_list
+    except Exception as e:
+        print("Ошибка при получении списка процессов:", e)
+        return None
 
-if __name__ == "__main__":
-    multi_core_procs = get_multi_core_processes()
-    if multi_core_procs:
-        print("Processes using more than 2 cores:")
-        for proc in multi_core_procs:
-            print(f"PID: {proc.info['pid']}, Name: {proc.info['name']}, CPU Affinity: {proc.info['cpu_affinity']}, Location: {proc.info['exe']}")
-    else:
-        print("No processes found using more than 2 cores.")
+# Получаем список процессов
+process_list = get_linux_processes()
+
+# Выводим список процессов
+if process_list:
+    for process in process_list:
+        print(process)
+else:
+    print("Не удалось получить список процессов.")
