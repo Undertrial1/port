a1943de1d69d283c2ba799a3462c64d3579b5ac5
fintafixuser <164501103+fintafixuser@users.noreply.github.com>
2024-04-17 16:09:20 +0300

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -2,7 +2,7 @@ import psutil
 
 def get_multi_core_processes():
     multi_core_processes = []
-    for proc in psutil.process_iter(['pid', 'name', 'cpu_affinity']):
+    for proc in psutil.process_iter(['pid', 'name', 'cpu_affinity', 'exe']):
         cpu_affinity = proc.info['cpu_affinity']
         if cpu_affinity and len(cpu_affinity) > 2:
             multi_core_processes.append(proc)
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     if multi_core_procs:
         print("Processes using more than 2 cores:")
         for proc in multi_core_procs:
-            print(f"PID: {proc.info['pid']}, Name: {proc.info['name']}, CPU Affinity: {proc.info['cpu_affinity']}")
+            print(f"PID: {proc.info['pid']}, Name: {proc.info['name']}, CPU Affinity: {proc.info['cpu_affinity']}, Location: {proc.info['exe']}")
     else:
         print("No processes found using more than 2 cores.")
